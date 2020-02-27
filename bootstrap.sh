aeebb965df95b6ed66f1b0b76181afd6af0f7403
Emanuel Joívo <emanueljoivo@gmail.com>
2020-02-26 22:35:13 -0300

    Fix aspects location

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function _setup_vim(){
-    cat aspects.vim | tee ~/.vimrc
+    cat vim/aspects.vim | tee ~/.vimrc
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 }
