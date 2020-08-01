f789e0c4c0c2962cdec643b81396956890fd1964
Emanuel Joivo <emanueljoivo@gmail.com>
2020-08-01 13:43:44 -0300

    Add bashrc config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
-function _setup_vim(){
-    cat vim/aspects.vim | tee ~/.vimrc
+set -e -x
+
+replace_bashrc() {
+    cat .bashrc >> ~/.bashrc
+}
+
+
+setup_vim() {
+    cat vim/aspects.vim >> ~/.vimrc
+    rm -rf ~/.vim/bundle/Vundle.vim
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 }
 
-_setup_vim
+install_vim() {
+    sudo apt-get -y -qq install vim 1>/dev/null
+    setup_vim    
+}
+
+main() {
+    replace_bashrc
+    install_vim
+}
+
+main
